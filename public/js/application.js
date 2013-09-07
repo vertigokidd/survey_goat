1417de3f19b67e4b248a05c24c155295f6bc6cf6
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 22:38:53 -0500

    working js

--- public/js/application.js
+++ public/js/application.js
@@ -1,27 +1,54 @@
 var questionCounter = 2
+var question;
 
-function addNewQuestion(argument) {
-  // body...
+
+function addNewQuestion() {
+  questionClone= question.clone();
+
+  $(questionClone).children('span').text(questionCounter+'.');
+  $(questionClone).children('textarea').attr('name','question_'+ questionCounter+"[content]");
+  $(questionClone).children('input:first').attr('name', 'question_'+ questionCounter+'[choice_1]');
+  $(questionClone).children('input:last').attr('name', 'question_'+ questionCounter+'[choice_2]');
+  $(questionClone).children('#questionBox:nth-child(4)').attr('name', 'question_'+ questionCounter+'[choice_2]');
+
+  $(questionClone).insertBefore("#choice");
+}
+
+function addNewChoice () {
+  choices = $(".questionBox:last input");
+  choiceClone = $(choices).last().clone();
+  questionNumber = $(".questionBox:last").children('span').text().slice(0,1);
+  choiceCounter = choices.length + 1
+
+  choiceClone.attr('name', 'question_'+questionNumber+'[choice_'+choiceCounter+']');
+
+  $(choiceClone).appendTo(".questionBox:last");
+}
+
+function questionTemplate () {
+  question = $(".questionBox").clone();
 }
 
 
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  questionTemplate();
 
+  $('#question').on('click', function(event) {
+    event.preventDefault();
 
-$('#question').on('click', function(event) {
-  event.preventDefault();
+    addNewQuestion();
 
-  addNewQuestion();
-
-});
+    questionCounter++;
+  });
 
 
+  $('#choice').on('click', function(event){
+    event.preventDefault();
 
+    addNewChoice();
+
+  });
 
 
 
