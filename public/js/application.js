04b285574d6181d0c234642c8f0ee1ffd9b3fb66
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 19:14:52 -0500

    Make profile and survey pages dynamic

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,29 @@
+var questionCounter = 2
+
+function addNewQuestion(argument) {
+  // body...
+}
+
+
 $(document).ready(function() {
   // This is called after the document has loaded in its entirety
   // This guarantees that any elements we bind to will exist on the page
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+
+$('#question').on('click', function(event) {
+  event.preventDefault();
+
+  addNewQuestion();
+
+});
+
+
+
+
+
+
+
 });
